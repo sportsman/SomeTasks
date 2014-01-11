607a6dc3d6ca8d08eb208f741795985304af68b8
sportsman <wazzaappp.1994@yahoo.com>
2014-01-11 14:22:16 +0200

    .init

---
+++ 04.UKFlag/UKFlag.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class UKFlag
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int middleDots = N/2-1;
+
+        for (int i = 1; i <= N/2; i++)
+        {
+            int leftAndRightDots = i - 1;
+            Console.Write(new string('.', leftAndRightDots));
+            Console.Write("\\");
+            Console.Write(new string('.', middleDots));
+            Console.Write("|");
+            Console.Write(new string('.', middleDots));
+            Console.Write("/");
+            Console.Write(new string('.', leftAndRightDots));
+            middleDots--;
+            Console.WriteLine();
+        }
+
+        int numberOfDashes = N / 2;
+
+        Console.Write(new string('-',numberOfDashes));
+        Console.Write("*");
+        Console.Write(new string('-', numberOfDashes));
+        Console.WriteLine();
+
+        middleDots = 0;
+
+        for (int i = N/2; i >= 1; i--)
+        {
+            int leftAndRightDots = i - 1;
+            Console.Write(new string('.', leftAndRightDots));
+            Console.Write("/");
+            Console.Write(new string('.', middleDots));
+            Console.Write("|");
+            Console.Write(new string('.', middleDots));
+            Console.Write("\\");
+            Console.Write(new string('.', leftAndRightDots));
+            middleDots++;
+            Console.WriteLine();
+        }
+
+    }
+}
+
